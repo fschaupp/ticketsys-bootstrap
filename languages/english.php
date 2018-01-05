ef176c8aac324ee20fb5b9cdd752e01926f6462b
Florian Schaupp <fschaupp@hotmail.com>
2018-01-05 14:56:23 +0100

    added english language

---
+++ languages/english.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dafnik
+ * Date: 27.12.17
+ * Time: 17:20
+ */
+
+$string_title_first = 'Tickets - ';
+$string_title_home = 'Home';
+$string_title_movieManagement = 'Moviemanager';
+$string_title_userManagement = 'Usermanager';
+$string_title_cardService = 'Cinema Service';
+
+$string_cancel = 'Cancel';
+
+
+$string_home_date = 'Date:';
+
+$string_home_trailer = 'Trailer:';
+$string_home_trailer_button_tooltip = 'Open Trailer';
+
+$string_home_cardService = 'Cinema Service:';
+$string_home_cardService_button_tooltip_already_showed = 'That movie was already shown';
+$string_home_cardService_button_text = 'check in';
+$string_home_cardService_button_tooltip = 'enter yourself for the cinemaservice';
+$string_home_cardService_remove_button_tooltip = 'remove yourself for the cinemaservice';
+$string_home_cardService_no_one_assigned = 'nobody assigned!';
+$string_home_emergencyCardService = 'cinemaservice: <br>(just in case)</br>';
+$string_home_emergencyCardService_button_tooltip = $string_home_cardService_button_tooltip . ' (just in case)';
+
+$string_home_available_cards = 'available tickets:';
+
+$string_home_booked_cards = 'reserved tickets:';
+
+$string_home_booking_fully_booked = 'no tickets left';
+$string_home_booking_fully_booked_tooltip = 'Sorry, there are no available tickets';
+$string_home_booking_already_been_shown = 'passed';
+$string_home_booking_already_been_shown_tooltip = 'This movie was already shown';
+$string_home_booking = 'book tickets';
+$string_home_booking_tooltip = 'You aren\'t signed in!';
+
+$string_home_cancel_booking = 'cancle booking';
+
+$string_home_modal_book_cards_title = 'reserved tickets - available tickets: ';
+$string_home_modal_book_cards_available_cards = 'available tickets: ';
+$string_home_modal_book_cards_alert = '<strong>Attention!</strong> These tickets are primarily for members of the Bürgerkorps Eggenburg. 
+In case of available tickets shortly bevore the film show starts, friends or family of members can also get some!';
+$string_home_modal_book_cards_number_of_cards = 'ticketscount: ';
+$string_home_modal_book_cards_button_success = 'send booking';
+
+$string_home_modal_get_in_touch_title_1 = 'Do you really want to do the ticket service at ';
+$string_home_modal_get_in_touch_title_2 = ' ?';
+$string_home_modal_get_in_touch_alert = '<strong>Attention!</strong> You can revert this step at any time!';
+$string_home_modal_get_in_touch_button_success = 'check in';
+
+$string_home_modal_cancel_get_in_touch_title_1 = 'Do you really want to check out for';
+$string_home_modal_cancel_get_in_touch_title_2 = ' ?';
+$string_home_modal_cancel_get_in_touch_button_success = 'check out';
+
+$string_home_modal_cancel_booked_cards_title_1 = 'Do you really want to cancle the booking for ';
+$string_home_modal_cancel_booked_cards_title_2 = ' ?';
+$string_home_modal_cancel_booked_cards_alert = '<strong>Attention!</strong> If you booked several tickets, all of them will be cancled!';
+$string_home_modal_cancel_booked_cards_button_success = 'cancle';
+
+
+$string_movieManagement_header = 'All Movies';
+$string_movieManagement_subheader = 'Here you can list, add, remove and edit all movies.';
+$string_movieManagement_button_create_movie = 'add movie';
+
+$string_movieManagement_table_header_movie_name = 'Title';
+$string_movieManagement_table_header_booked_cards = 'booked tickets';
+$string_movieManagement_table_header_date = 'date';
+$string_movieManagement_table_header_trailer = 'trailer';
+$string_movieManagement_table_header_worker = 'ticket service';
+$string_movieManagement_table_header_worker_emergency = 'ticket service (just in case)';
+$string_movieManagement_table_header_actions = 'actions';
+
+$string_movieManagement_table_body_no_worker = 'no ticket service';
+$string_movieManagement_table_body_trailer = 'trailer';
+$string_movieManagement_table_body_actions_edit = 'edit';
+$string_movieManagement_table_body_actions_delete = 'delete';
+
+$string_movieManagement_modal_movie_name = 'title:';
+$string_movieManagement_modal_date = 'date: ';
+$string_movieManagement_modal_trailer = 'trailer: ';
+
+$string_movieManagement_modal_create_movie_title = 'add a new movie:';
+$string_movieManagement_modal_create_movie_success = 'add';
+
+$string_movieManagement_modal_edit_movie_title = 'save movie: ';
+$string_movieManagement_modal_edit_movie_success = 'save';
+
+$string_movieManagement_modal_delete_movie_title = 'Do yo really want to delete this movie?';
+$string_movieManagement_modal_delete_movie_success = 'delete';
+
+
+$string_userManagement_header = 'all users';
+$string_userManagement_subheader = 'Here you can find all registered users';
+
+$string_userManagement_table_header_firstname_and_surname = 'name';
+$string_userManagement_table_header_email = 'Email-Address';
+$string_userManagement_table_header_rank = 'priviledge';
+$string_userManagement_table_header_actions = 'actions';
+
+$string_userManagement_table_body_actions_change_password = 'change password';
+$string_userManagement_table_body_actions_delete = 'delete';
+
+$string_userManagement_modal_edit_password_title = 'change password:';
+$string_userManagement_modal_edit_password_password = 'password:';
+$string_userManagement_modal_edit_password_confirm = 'confirm password:';
+$string_userManagement_modal_edit_password_success = 'update';
+
+$string_userManagement_modal_edit_rank_title = 'change priviledge: ';
+$string_userManagement_modal_edit_rank_rank = 'priviledge:';
+$string_userManagement_modal_edit_rank_success = 'update';
+
+$string_userManagement_modal_delete_user_title = 'Do you really want to delete this user?';
+$string_userManagement_modal_delete_user_success = 'delete';
+
+
+$string_card_service_header = 'Ticketservice';
+$string_card_service_subheader = 'Here you can find all booked cards for your ticketservice.';
+
+$string_card_service_table_header_movie_name = 'name';
+$string_card_service_table_header_booked_cards = 'card count';
+
+$string_card_service_there_are_no_bookings = 'Ohh... No one wants to see this movie :/';
